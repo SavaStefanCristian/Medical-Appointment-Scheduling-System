bc702faa31d8b7013af95bf127ce575221f112c9
RotarRobert183 <rotarmartin95@gmail.com>
2026-04-20 23:09:58 +0300

    unused variable

--- Medical-Appointment-Scheduling-System-App/frontend/src/app/pages/register/register.ts
+++ Medical-Appointment-Scheduling-System-App/frontend/src/app/pages/register/register.ts
@@ -24,7 +24,7 @@ export class Register {
   register() {
     const dto: RegisterDto = { name: this.name, phone: this.phone, email: this.email, password: this.password };
     this.authService.register(dto).subscribe({
-      next: res => {
+      next: () => {
         this.router.navigate(['/login']);
       },
       error: () => {
